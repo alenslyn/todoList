cffd5a3a4a1e3766a286b1fbfc0a46e5d22c36d0
alenslyn <alenslyn@gmail.com>
2023-11-21 12:27:10 +0100

    feat: add CancelButton

--- src/components/modal/CancelButton.jsx
+++ src/components/modal/CancelButton.jsx
@@ -1,10 +1,14 @@
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import { useDispatch } from "react-redux";
+import { hideModal } from "../../redux/modalSlice";
 
 export default function CancelButton() {
+  const dispatch = useDispatch();
   return (
     <Stack spacing={2} direction="row">
       <Button
+        onClick={() => dispatch(hideModal())}
         style={{ background: "rgb(108,99,255)", color: "white" }}
         variant="outlined"
       >

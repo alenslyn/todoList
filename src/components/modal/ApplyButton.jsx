55f8455a591927f7dde07eb1f32c9b76f59c3084
alenslyn <alenslyn@gmail.com>
2023-11-22 16:03:13 +0100

    feat: add addtodo action to apply button

--- src/components/modal/ApplyButton.jsx
+++ src/components/modal/ApplyButton.jsx
@@ -2,13 +2,17 @@ import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { useDispatch } from "react-redux";
 import { hideModal } from "../../redux/modalSlice";
+import { addTodo } from "../../redux/todoSlice";
 
 export default function ApplyButton() {
   const dispatch = useDispatch();
   return (
     <Stack spacing={2} direction="row">
       <Button
-        onClick={() => dispatch(hideModal())}
+        onClick={() => {
+          dispatch(hideModal());
+          dispatch(addTodo());
+        }}
         style={{ background: "rgb(108,99,255)", color: "white" }}
         variant="outlined"
       >
